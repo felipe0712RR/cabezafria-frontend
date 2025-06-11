b11e8f29ae2c04fe98b13f3fa1dff4eca06cc62c
felipe <felipez111946@gmail.com>
2025-06-11 18:41:11 -0500

    [+] Se agregan los componentes publicos home/register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/public/home/home';
+import { Login } from './pages/public/login/login';
+import { Register } from './pages/public/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    { path: 'home', component: Home },
+    { path: 'login', component: Login},
+    { path:'registe', component: Register},
+    { path: '**', redirectTo: 'home',pathMatch: 'full'},
+    { path: '', redirectTo: 'home',pathMatch: 'full'}
+];
