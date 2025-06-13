f095716b3b9252f583995a1ef6c7c8417b6fad1c
daannn88 <celydaniel97@gmail.com>
2025-06-12 20:32:08 -0500

    [+]Se agrega el cuerpo del header y el footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/public/home/home';
 import { Login } from './pages/public/login/login';
 import { Register } from './pages/public/register/register';
+import { Products } from './pages/private/products/products';
 
 export const routes: Routes = [ 
     { path: 'home', component: Home },
     { path: 'login', component: Login},
-    { path:'registe', component: Register},
+    { path:'register', component: Register},
+    { path: 'dashboard/products', component: Products},
     { path: '**', redirectTo: 'home',pathMatch: 'full'},
     { path: '', redirectTo: 'home',pathMatch: 'full'}
 ];
