5aa381a79d41eccafcb598de1608a35099f69f7f
felipe <felipez111946@gmail.com>
2025-07-05 09:12:04 -0500

    [+] Se crea rama featute/favourite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/public/home/home';
 import { Login } from './pages/public/login/login';
 import { Register } from './pages/public/register/register';
-import { Favourite } from './pages/private/users/new-form/favourite/favourite';
+import { fovourite } from './pages/private/users/new-form/fovourite/fovourite';
 
 export const routes: Routes = [ 
     { path: 'home', component: Home },
     { path: 'login', component: Login},
     { path:'registe', component: Register},
-    { path:'favourite', component: Favourite},
+    { path:'favourite', component: fovourite},
     { path: '**', redirectTo: 'home',pathMatch: 'full'},
     { path: '', redirectTo: 'home',pathMatch: 'full'}
 ];
