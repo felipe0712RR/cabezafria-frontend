d15955b5a5a24dcac0b79cf68a325d8f3fc62c5b
daannn88 <celydaniel97@gmail.com>
2025-07-07 18:59:09 -0500

    [+]Se soluciona error en el login, [+]Se implemeta el cuerpo del home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { Login } from './pages/public/login/login';
 import { Register } from './pages/public/register/register';
 import { Products } from './pages/private/products/products';
 import { ProductNewForm } from './pages/private/products/new-form/new-form';
+import { Dashboard } from './pages/private/dashboard/dashboard';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [ 
     { path: 'home', component: Home },
     { path: 'login', component: Login},
     { path: 'register', component: Register},
+    { path: 'dashboard', component: Dashboard, canActivate: [authGuard]},
     { path: 'dashboard/products', component: Products},
     { path: 'dashboard/products/new', component: ProductNewForm},
     { path: '**', redirectTo: 'home',pathMatch: 'full'},
