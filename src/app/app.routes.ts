4c07db690f2e8e09ed3f8d3a2f2b04fa3f826a25
Kenner Buitrago <kabconeesama@gmail.com>
2025-07-16 12:10:23 -0500

    Se agrega dashboard, rutas, y servicio de cartshopping que estan en construccion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,6 @@ import { CreateNewCategory } from './pages/private/category/new-category/new-cat
 import { GetProducts } from './pages/private/products/get-products';
 import { CreateNewProduct } from './pages/private/products/new-product/new-product';
 import { GetUsers } from './pages/private/users/get-users';
-import { Footer } from './components/layout/footer/footer';
 import { authGuard } from './guards/auth-guard';
 import { roleGuard } from './guards/role-guard';
 
@@ -19,12 +18,20 @@ export const routes: Routes = [
     { path: 'login', component: LoginUser },
     { path: 'register', component: CreateNewUser },
     { path: 'dashboard', component: AdminDashboard, canActivate: [authGuard, roleGuard], data: { expectedRoles: ['Administrador'] } },
+    { path: 'dashboard/categories/new', loadChildren: () => import('./pages/private/category/new-category/new-category').then(m => m.CreateNewCategory) },
+    { path: 'dashboard/products/new', loadChildren: () => import('./pages/private/products/new-product/new-product').then(m => m.CreateNewProduct) },
+    { path: 'dashboard/users', loadChildren: () => import('./pages/private/users/get-users').then(m => m.GetUsers) },
+    //{ path: 'page4', loadChildren: () => import('./pages/page4/page4.module').then(m => m.Page4Module) },
+    //{ path: 'page5', loadChildren: () => import('./pages/page5/page5.module').then(m => m.Page5Module) },
+   // { path: 'page6', loadChildren: () => import('./pages/page6/page6.module').then(m => m.Page6Module) },
     { path: 'dashboard/categories', component: GetCategories },
     { path: 'dashboard/categories/new', component: CreateNewCategory, canActivate: [authGuard, roleGuard], data: { expectedRoles: ['Administrador'] } },
     { path: 'dashboard/products', component: GetProducts },
     { path: 'dashboard/products/new', component: CreateNewProduct, canActivate: [authGuard, roleGuard], data: { expectedRoles: ['Administrador'] } },
-    { path: 'dashboard/users', component: GetUsers },
-    // { path: 'home', component: Footer },
+    { path: 'dashboard/users', component: GetUsers, canActivate: [authGuard, roleGuard], data: { expectedRoles: ['Administrador'] }},
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ]
+
+
+
