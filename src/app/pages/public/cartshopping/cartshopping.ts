9896a65e31885cb213fa2a41d353f6a68013df71
Kenner Alejandro Buitrago Castillo <kabconeesama@gmail.com>
2025-07-16 12:12:52 -0500

    se crea componente de cartshopping

---
+++ src/app/pages/public/cartshopping/cartshopping.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartshopping',
+  imports: [],
+  templateUrl: './cartshopping.html',
+  styleUrl: './cartshopping.css'
+})
+export class Cartshopping {
+
+}
